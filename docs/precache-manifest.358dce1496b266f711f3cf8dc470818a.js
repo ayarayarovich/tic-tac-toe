4458f48a98a63e4e0047e463ea27b0e03970ce28
Ayyar Khalilov <superayarcsgo2017@gmail.com>
2020-02-29 21:51:46 +0300

    added TicTacToe and all game's logic

---
+++ docs/precache-manifest.358dce1496b266f711f3cf8dc470818a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+    {
+        "revision": "b8df503cfea3356a2508ef519b3feffa",
+        "url": "/tic-tac-toe/index.html"
+    },
+    {
+        "revision": "c368fc6bebffa1c8aa4b",
+        "url": "/tic-tac-toe/static/css/2.1d10ae30.chunk.css"
+    },
+    {
+        "revision": "a1fc6c66a7196e40344e",
+        "url": "/tic-tac-toe/static/css/main.b3157edc.chunk.css"
+    },
+    {
+        "revision": "c368fc6bebffa1c8aa4b",
+        "url": "/tic-tac-toe/static/js/2.41a07f2d.chunk.js"
+    },
+    {
+        "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+        "url": "/tic-tac-toe/static/js/2.41a07f2d.chunk.js.LICENSE.txt"
+    },
+    {
+        "revision": "a1fc6c66a7196e40344e",
+        "url": "/tic-tac-toe/static/js/main.49f78ed9.chunk.js"
+    },
+    {
+        "revision": "e2033292bdf40307058b",
+        "url": "/tic-tac-toe/static/js/runtime-main.423d23ef.js"
+    }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e75daa2bc54d16fe51370b775e9d67c.js"
+  "/tic-tac-toe/precache-manifest.358dce1496b266f711f3cf8dc470818a.js"
 );
 
 self.addEventListener("message", (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-toe/index.html"), {
 
     blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/]
 });
